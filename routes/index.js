0d6700c5c7e486fd9432c4ca31c72d53f8e13141
Nox <benaziz.ines@gmail.com>
2022-03-23 23:37:43 +0100

    iterations 1 2 and 3 done, log out bugging

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,50 @@
-const router = require("express").Router();
+const bcryptjs = require("bcryptjs")
+const saltRounds = 10
+
+const router = require("express").Router()
+const User = require('../models/User.model')
+const isLoggedIn = require('../middlewear/isLoggedIn')
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
+router.get("/", (req, res) => {
+  res.render("index")
 });
 
-module.exports = router;
+router.get('/auth/login', (req, res) => {
+  res.render('auth/login')
+})
+
+router.post("/auth/login", (req, res, next) => {
+  const user = req.body
+  bcryptjs
+    .genSalt(saltRounds)
+    .then(salt => bcryptjs.hash(user.password, salt))
+    .then(hashedPassword => {
+      return User.create({
+        username: user.username,
+        password: hashedPassword})
+    })
+    .then(userFromDB => {
+      req.session.currentUser = user;
+      res.redirect('/')
+      console.log('Newly created user is: ', userFromDB);
+    })
+    .catch(error => next(error));
+})
+
+router.post('/logout', (req, res, next) => {
+  req.session.destroy(err => {
+    if (err) next(err)
+    res.redirect('/')
+  })
+})
+
+router.get('/main', isLoggedIn, (req, res, next) => {
+  res.render('auth/main')
+})
+
+router.get('/private', isLoggedIn, (req, res, next) => {
+  res.render('auth/private')
+})
+
+module.exports = router
